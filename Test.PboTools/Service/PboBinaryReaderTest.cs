df15ae8bc59ecebeb0937af7c901ca787666f20c
winse <win.accuracy@gmail.com>
2016-07-31 10:46:14 +0300

    Added unit tests for the PboBinaryReader and Writer

---
+++ Test.PboTools/Service/PboBinaryReaderTest.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using NUnit.Framework;
+using PboTools.Service;
+
+namespace Test.PboTools.Service
+{
+    public class PboBinaryReaderTest
+    {
+        public PboBinaryReader GetReader(Stream stream)
+        {
+            return new PboBinaryReader(stream);
+        }
+
+        [Test]
+        public void Test_ReadNullTerminatedString_Returns_The_String()
+        {
+            var bytes = new byte[] {0x71, 0x77, 0x65, 0x72, 0x74, 0x79, 0x00, 0x75, 0x69, 0x6f, 0x70};//qwerty0uiop
+            using (var stream = new MemoryStream(bytes))
+            {
+                PboBinaryReader reader = this.GetReader(stream);
+                string str = reader.ReadNullTerminatedString();
+                Assert.AreEqual("qwerty", str);
+            }
+        }
+    }
+}
---
+++ Test.PboTools/Service/PboBinaryWriterTest.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using NUnit.Framework;
+using PboTools.Service;
+
+namespace Test.PboTools.Service
+{
+    public class PboBinaryWriterTest
+    {
+        private PboBinaryWriter GetWriter(Stream stream)
+        {
+            return new PboBinaryWriter(stream);
+        }
+
+        [Test]
+        public void Test_WriteNullTerminatedString_Writes_The_String()
+        {
+            var buffer = new byte[] {0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 };
+            using (var stream = new MemoryStream(buffer))
+            {
+                PboBinaryWriter writer = this.GetWriter(stream);
+                writer.WriteNullTerminatedString("qwerty");
+                writer.Flush();
+
+                var expected = new byte[] { 0x71, 0x77, 0x65, 0x72, 0x74, 0x79, 0x00, 0x01, 0x01, 0x01 };
+                CollectionAssert.AreEqual(expected, buffer);
+            }
+        }
+    }
+}
